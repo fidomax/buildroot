1f3b2bf71719825117978447b9e1fcf8770a605a
Maxim Kochetkov <fido_max@inbox.ru>
2017-02-28 11:50:19 +0300

    + startup script * defconfig updated (canmon, locales)

--- package/openscada/openscada.mk
+++ package/openscada/openscada.mk
@@ -14,4 +14,9 @@ OPENSCADA_DEPENDENCIES = gd zlib sqlite openssl lm-sensors fftw host-pkgconf
 OPENSCADA_AUTORECONF = YES
 OPENSCADA_CONF_OPTS += --enable-CrossCompile --disable-FireBird --disable-PostgreSQL --disable-MySQL --disable-SoundCard --disable-SNMP --disable-DiamondBoards --disable-QTCfg --disable-QTStarter --disable-Vision --enable-FT3 --enable-MSO
 
+define OPENSCADA_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 package/openscada/S60openscada \
+		$(TARGET_DIR)/etc/init.d/S60openscada
+endef
+
 $(eval $(autotools-package))
